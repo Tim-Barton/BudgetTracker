f418c4c75afd620a2ef6dc80651b7a1d11754b54
Tim Barton <timothyebarton@gmail.com>
2020-01-03 17:06:28 +1000

    Refactoring settings_helper.py pulling out the Categories classes

---
+++ src/categories.py
@@ -0,0 +1,57 @@
+import re
+
+
+class CategoryManager:
+    def __init__(self, inputCategories):
+        self.__categoriesList = []
+        self.addCategory("Unknown")
+        self.addRegexToCategory("Unknown", "Unknown")
+        if inputCategories is not None:
+            for category in inputCategories:
+                name = category["Category"]
+                regexes = category["Regexes"]
+                self.addCategory(name)
+                for regex in regexes:
+                    self.addRegexToCategory(name, regex)
+
+    def dump(self):
+        returnList = []
+        for category in self.__categoriesList:
+            thisDict = {}
+            thisDict["Category"] = category.name
+            thisDict["Regexes"] = category.getRegexes()
+            returnList.append(thisDict)
+        return returnList
+
+    def addCategory(self, name):
+        if name not in self.getCategoryNames():
+            self.__categoriesList.append(Category(name))
+
+    def addRegexToCategory(self, categoryName, regex):
+        [x for x in self.__categoriesList if x.name
+            == categoryName][0].addRegex(regex)
+
+    def getRegexes(self):
+        return [x.regexes for x in self.__categoriesList]
+
+    def getCategoryNames(self):
+        return [x.name for x in self.__categoriesList]
+
+    def getRegexesAsList(self):
+        return [regex for regexes in self.getRegexes() for regex in regexes]
+
+    # returns the first category that has the appropriate regex
+    def getCategoryFromRegex(self, regex):
+        return [x.name for x in self.__categoriesList if regex in x.regexes][0]
+
+
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.regexes = set([])
+
+    def addRegex(self, regex):
+        self.regexes.add(re.escape(regex))
+
+    def getRegexes(self):
+        return list(self.regexes)
--- src/settings_handler.py
+++ src/settings_handler.py
@@ -4,7 +4,7 @@ Created on 10 Aug 2017
 @author: Tim Barton
 '''
 import json
-import re
+import categories
 
 
 def ParseSettings(config):
@@ -14,7 +14,7 @@ def ParseSettings(config):
     if "Categories" in configJson.keys():
         categoriesJson = configJson["Categories"]
 
-    categoryManager = CategoryManager(categoriesJson)
+    categoryManager = categories.CategoryManager(categoriesJson)
 
     dataConfigs = {}
     if "Configuration" in configJson.keys():
@@ -36,62 +36,6 @@ def WriteSettings(categoryManager, dataConfigs):
     config["Configuration"] = dataConfigJson
 
     return json.dumps(config, indent=2)
-
-
-class CategoryManager:
-    def __init__(self, inputCategories):
-        self.__categoriesList = []
-        self.addCategory("Unknown")
-        self.addRegexToCategory("Unknown", "Unknown")
-        if inputCategories is not None:
-            for category in inputCategories:
-                name = category["Category"]
-                regexes = category["Regexes"]
-                self.addCategory(name)
-                for regex in regexes:
-                    self.addRegexToCategory(name, regex)
-
-    def dump(self):
-        returnList = []
-        for category in self.__categoriesList:
-            thisDict = {}
-            thisDict["Category"] = category.name
-            thisDict["Regexes"] = category.getRegexes()
-            returnList.append(thisDict)
-        return returnList
-
-    def addCategory(self, name):
-        if name not in self.getCategoryNames():
-            self.__categoriesList.append(Category(name))
-
-    def addRegexToCategory(self, categoryName, regex):
-        [x for x in self.__categoriesList if x.name
-            == categoryName][0].addRegex(regex)
-
-    def getRegexes(self):
-        return [x.regexes for x in self.__categoriesList]
-
-    def getCategoryNames(self):
-        return [x.name for x in self.__categoriesList]
-
-    def getRegexesAsList(self):
-        return [regex for regexes in self.getRegexes() for regex in regexes]
-
-    # returns the first category that has the appropriate regex
-    def getCategoryFromRegex(self, regex):
-        return [x.name for x in self.__categoriesList if regex in x.regexes][0]
-
-
-class Category:
-    def __init__(self, name):
-        self.name = name
-        self.regexes = set([])
-
-    def addRegex(self, regex):
-        self.regexes.add(re.escape(regex))
-
-    def getRegexes(self):
-        return list(self.regexes)
 
 
 class DataFileConfig:
